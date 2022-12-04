86dac42e4fe036f0e2a05df831ad9d59c9e4da65
Martin Poblete <75780095+Pobletem@users.noreply.github.com>
2022-12-04 10:39:08 -0600

    Change form id name and add js funciton for checking form filled status

---
+++ js/site.js
@@ -0,0 +1,11 @@
+function checkRequirements() {
+  
+  var name = document.forms["contactForm"]["name_field"].value;
+  var email = document.forms["contactForm"]["email_field"].value;
+  
+  if ((name == "") && (email == "")) {
+    document.getElementById("error").style.display = "block";
+  } else {
+    alert("Sign up successful");
+  }
+}
